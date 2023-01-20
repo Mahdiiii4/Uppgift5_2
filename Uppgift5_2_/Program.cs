892184e851080df25e657e1992aed4c8920918d1
Admin <Admin@TEB127007>
2023-01-20 10:28:52 +0100

    Add project files.

---
+++ Uppgift5_2_/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int svar = int.Parse(Console.ReadLine());
+            int[] tal = new int[svar];
+            for (int i = 0; i < svar; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < svar; i++)
+            {
+                Console.WriteLine("Den tal är:");
+                int nummer;
+                nummer = i + 1;
+                Console.WriteLine("tal" + nummer + ": "+tal[i] + "");
+            }
+        }
+    }
+}
